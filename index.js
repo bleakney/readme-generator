a0975e8b646a0aca182044caf5888626cefb01e8
Bleakney Bissett <bleakneybissett@gmail.com>
2021-07-11 19:05:49 -0500

    testing README

--- index.js
+++ index.js
@@ -1,13 +1,103 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'What is the name of your project? (Required)',
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a project name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'projectDescription',
+            message: 'Please enter a description for your project. (Required)',
+            validate: projectDescriptionInput => {
+                if (projectDescriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmInstallationInstructions',
+            message: 'Does your project require installation instructions?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'installationInstructions',
+            message: "Please enter the installation instructions for your project.",
+            when: ({ confirmInstallationInstructions }) => confirmInstallationInstructions
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please enter any usage information for your project.'
+        },
+        {
+            type: 'confirm',
+            name: 'confirmContributing',
+            message: 'Does your project allow other users to contribute to its ongoing development?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Please describe the Contribution Guidelines for your project.',
+            when: ({ confirmContributing }) => confirmContributing
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please enter any pertinent information regarding testing for your project.'
+        },
+        {
+            type: 'input',
+            name: 'githubUsername',
+            message: 'What is your github username?',
+        },
+        {
+            type: 'input',
+            name: 'userEmail',
+            message: 'What is your email address?'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please select a license for your project. (Required)',
+            choices: ['MIT', 'Apache', 'GPL']
+        }
+    ])};
 
-// TODO: Create a function to initialize app
-function init() {}
+
+// Function to initialize app
+async function init() {
+    promptUser()
+    .then(answers => {
+        const markdownContent = generateMarkdown(answers);
+        
+        fs.writeFile('./README.md', markdownContent, err => {
+            if (err) throw new Error(err);
+            console.log('README created! Check out README.md in this directory to see it!')
+        } )
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,65 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {
+//   if(license === 'MIT') {
+//     answers.push({licenseLink: 'https://opensource.org/licenses/MIT'});
+//   } else if (license === 'Apache') {
+//     answers.push({licenseLink})
+//   }
+// }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  console.log(answers);
+
+  return `
+  # ${answers.projectTitle}
+  [![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)
+
+  ## Table of Contents
+  * [Description] (#description)
+  * [Installation] (#installation)
+  * [Usage] (#usage)
+  * [Contribution Guidelines] (#contribution)
+  * [Testing] (#testing)
+  * [Questions] (#questions)
+  
+  ## Description
+
+  ${answers.projectDescription}
+
+  ## Installation
+
+  ${answers.installationInstructions}
+
+  ## Usage
+
+  ${answers.usage}
+
+  ## Contribution Guidelines
+
+  ${answers.contributing}
+
+  ## Testing
+
+  ${answers.tests}
+
+  ## Questions
+
+  ### Where to find me:
+  [My Github] (http://www.gihub.com/${answers.githubUsername})
+
+  ### Where to reach me:
+  [Email] (mailto:${answers.userEmail}
 
 `;
 }
